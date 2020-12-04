0e775774a283f9f55b5ae99567027c2f8ce57fbd
XGlorot <75086114+XGlorot@users.noreply.github.com>
2020-12-04 16:28:08 -0500

    Update convert_vaccine_data.py

--- data/convert_vaccine_data.py
+++ data/convert_vaccine_data.py
@@ -4,6 +4,65 @@ import re
 name = ['DTaP', 'Polio', 'MMR', 'Hib', 'Varicelle',
         'HepatB', 'HepatA','PCV', 'Rotavirus']
 
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'American Samoa': 'AS',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'District of Columbia': 'DC',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Guam': 'GU',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Northern Mariana Islands':'MP',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Puerto Rico': 'PR',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virgin Islands': 'VI',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY'
+}
+
 for nn in name:
   sep = ";"
   offset = 2
@@ -60,7 +119,8 @@ for nn in name:
           assert(title[idx + 2] == 'SAMPLE SIZE')
           SIZE = float(parsed[idx + 2])
           idx = idx + 4
-        dd[(dose, ag, location, year)] = (value, LL, UL, SIZE)
+        if location in us_state_abbrev:
+            dd[(dose, ag, us_state_abbrev[location], year)] = (value, LL, UL, SIZE)
       else:
         idx = idx + 4
     c = f.readline()
